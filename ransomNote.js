5f393a1e1a169f8aa236715e60549dd7eebd066f
innocentemutabazi <innocentemutabazi@gmail.com>
2024-07-26 20:22:18 +0200

    .

--- ransomNote.js
+++ ransomNote.js
@@ -1,26 +1,25 @@
 var canConstruct = function (ransomNote, magazine) {
-let ransomObj = {};
-  let magazineObj = {};
-  for (let char of ransomNote) {
-    console.log(char)
-    if (!ransomObj[char]) {
-      ransomObj[char]=0;
-    }
-    ransomObj[char]++;
-  }
-  console.log(ransomObj)
-  for (let char of magazine) {
-    if (!magazineObj[char]) {
-      magazineObj[char]=0;
-    }
-    magazineObj[char]++;
-  }
-  console.log(magazineObj)
-  for (key in ransomObj) {
-    console.log(key)
-    if (ransomObj[key] != magazineObj[key]){
-    return false;}
-  }
-  return true;
-};
+    let ransomObj = {};
+      let magazineObj = {};
+      for (let char of ransomNote) {
+        if (!magazine.includes(char)) return false
+        if (!ransomObj[char]) {
+          ransomObj[char]=0;
+        }
+        ransomObj[char]++;
+      }
+      console.log(ransomObj)
+      for (let char of magazine) {
+        if (!magazineObj[char]) {
+          magazineObj[char]=0;
+        }
+        magazineObj[char]++;
+      }
+      for (key in ransomObj) {
+        console.log(key)
+        if (ransomObj[key]>magazineObj[key]){
+        return false;}
+      }
+      return true;
+    };
 console.log(canConstruct('aa','aab'))
